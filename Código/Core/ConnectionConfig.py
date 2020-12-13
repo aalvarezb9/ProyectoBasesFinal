aa1efd50b0996899e3c72fa775e3ec7506acf6b5
aalvarezb9 <60557058+aalvarezb9@users.noreply.github.com>
2020-12-13 11:10:23 -0600

    Update ConnectionConfig.py

--- Código/Core/ConnectionConfig.py
+++ Código/Core/ConnectionConfig.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+
+"""
+    ! Hace la conexión con la BD con los parámetros en config.ini
+       
+
+    @author: Ángel
+    @date: 13 \ 12 \ 2020 
+
+"""
+
 class ConnectionConfig:
 
     def __init__(self,server,port,user,password,database):
