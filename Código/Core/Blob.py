f4057f15afd7b4f06c1641df1346cb46cc832784
aalvarezb9 <60557058+aalvarezb9@users.noreply.github.com>
2020-12-13 11:09:28 -0600

    Update Blob.py

--- Código/Core/Blob.py
+++ Código/Core/Blob.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 
+"""
+    ! Clase que trabaja con binarios
+
+    @author: Ángel
+    @date: 13 \ 12 \ 2020 
+"""
+
 class Blob:
 
     def __init__(self, filename):
         self.filename = filename
     
+    # Convierte a binario
     def toBinary(self):
         with open(self.filename, 'rb') as file:
             binarydata = file.read()
 
         return binarydata
 
+    # Lo contrario a la función anterior
     def toFile(self, binarydata):
         with open(self.filename, 'wb') as file:
             file.write(binarydata)
