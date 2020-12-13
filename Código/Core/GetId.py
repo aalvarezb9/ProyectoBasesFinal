c4dfb76d28f6686866805c280c0cfdda89e39bb1
aalvarezb9 <60557058+aalvarezb9@users.noreply.github.com>
2020-12-13 11:11:53 -0600

    Update GetId.py

--- Código/Core/GetId.py
+++ Código/Core/GetId.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+
+"""
+    ! Obtiene el id del usuario activo que está guardado temporalmente en un archivo json
+        
+
+    @author: Ángel
+    @date: 13 \ 12 \ 2020 
+
+"""
+
 import os
 import json
 
