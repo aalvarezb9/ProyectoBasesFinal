76b42de768895aeeea5b7e698da1713255e0fb52
aalvarezb9 <60557058+aalvarezb9@users.noreply.github.com>
2020-12-13 11:12:39 -0600

    Update ManejoArchivos.py

--- Código/Core/ManejoArchivos.py
+++ Código/Core/ManejoArchivos.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+
+"""
+    ! Se hace la compresión de los archivos y su proceso inverso
+        * Esto se hace para enviar el archivo con ese formato a la BD B
+
+    @author: Ángel
+    @date: 13 \ 12 \ 2020 
+
+"""
+
 import gzip
 
 class Manejo:
